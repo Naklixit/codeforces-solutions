ba21cab0cf54b963c17f89d0f55592599cc76b64
Naklixit <liveontheword@gmail.com>
2026-01-19 16:42:33 +0700

    Upload A_System_of_Equations solution

---
+++ A_System_of_Equations.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int cnt = 0;
+    for (int a = 0; a <= sqrt(n); a++) {
+        int b = n - a * a;
+        if (b >= 0 && a + b * b == m) {
+            cnt++;
+        }
+    }
+    cout << cnt << endl;
+}
