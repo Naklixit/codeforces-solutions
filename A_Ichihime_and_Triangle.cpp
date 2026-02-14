844ea28bb43354f036e4a5f128337e1d7d9e2de1
Naklixit <liveontheword@gmail.com>
2026-02-14 09:32:28 +0700

    Add solution for A.Ichihime and Triangle problem

---
+++ A_Ichihime_and_Triangle.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        int x = a;
+        int y = c;
+        int z = c;
+        cout << x << " " << y << " " << z << endl;
+    }
+}
